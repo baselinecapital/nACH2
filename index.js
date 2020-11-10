28e9c428dbcb71f5d44ac73e4c63c0ca4efc5188
ellenaua <elenasharovar@gmail.com>
2020-11-10 19:24:50 +0200

    Bank Holidays

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ module.exports.Batch = require('./lib/batch');
 module.exports.File  = require('./lib/file');
 module.exports.Utils  = require('./lib/utils');
 module.exports.Validate  = require('./lib/validate');
+module.exports.BANK_HOLIDAYS  = require('./lib/bank-holidays/index');
